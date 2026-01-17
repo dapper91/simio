dapper91/python-aio

feat: first library release.

examples/buffered_stream.py
@@ -1,0 +1,24 @@
+import asyncio
+
+from aio import net, stream
+
+
+async def main() -> None:
+    async with await net.open_tcp_stream("httpforever.com", 80) as http_stream:
+        buffered_stream = stream.BufferedStream(http_stream)
+        await buffered_stream.write_all(
+            b'GET / HTTP/1.1\r\n'
+            b'Host: httpforever.com\r\n'
+            b'\r\n',
+        )
+        status = await buffered_stream.read_until(b'\r\n')
+        print(status.decode())
+
+        headers = await buffered_stream.read_until(b'\r\n\r\n')
+        headers = dict(line.split(b':', maxsplit=1) for line in headers.removesuffix(b'\r\n\r\n').split(b'\r\n'))
+
+        body = await buffered_stream.read_exactly(size=int(headers[b'Content-Length']))
+        print(body.decode())
+
+
+asyncio.run(main())

aio/net/tcp/server.py
@@ -1,0 +1,112 @@
+import asyncio
+import logging
+import socket as sc
+from types import TracebackType
+from typing import Any, Callable, Coroutine, Optional, Self
+
+from aio.net import utils
+
+logger = logging.getLogger(__package__)
+
+
+async def start_tcp_server(
+        host: str,
+        port: int,
+        handler: Callable[[sc.socket], Coroutine[None, None, None]],
+        *,
+        family: sc.AddressFamily = sc.AddressFamily.AF_INET,
+        backlog: int = 128,
+        reuse_address: bool = True,
+        graceful_shutdown: Optional[float] = None,
+) -> None:
+    """
+    Starts TCP socket server.
+
+    :param host: address to bind to
+    :param port: port to bind to
+    :param handler: client socket handler
+    :param family: address family
+    :param backlog: number of unaccepted connections that the system will allow before refusing new connections
+    :param reuse_address: reuse bind address
+    :param graceful_shutdown: period of time during which the server waits for handlers to complete
+                              before canceling them
+    """
+
+    loop = asyncio.get_running_loop()
+
+    logger.info("starting server on %s:%d", host, port)
+
+    with sc.socket(family, sc.SOCK_STREAM.SOCK_STREAM) as srv_socket:
+        if reuse_address:
+            srv_socket.setsockopt(sc.SOL_SOCKET, sc.SO_REUSEADDR, 1)
+
+        srv_socket.setblocking(False)
+        srv_socket.bind((host, port))
+        srv_socket.listen(backlog)
+
+        async with TaskGuard(graceful_shutdown=graceful_shutdown) as handlers:
+            while True:
+                try:
+                    cli_socket, address = await loop.sock_accept(srv_socket)
+                except asyncio.CancelledError:
+                    break
+
+                handlers.create_task(_handle_client_socket(handler, cli_socket))
+
+
+async def _handle_client_socket(handler: Callable[[sc.socket], Coroutine[None, None, None]], socket: sc.socket) -> None:
+    remote_host, remote_port = utils.get_socket_peer_address(socket)
+    logger.info("client %s:%d connected", remote_host, remote_port)
+
+    with socket:
+        try:
+            await handler(socket)
+        except Exception as e:
+            logger.exception("client %s:%d handling error: %s", remote_host, remote_port, e)
+
+    logger.info("client %s:%d disconnected", remote_host, remote_port)
+
+
+class TaskGuard:
+    def __init__(self, graceful_shutdown: Optional[float] = None):
+        self._graceful_shutdown = graceful_shutdown
+        self._tasks: set[asyncio.Task[None]] = set()
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+            self,
+            exc_type: Optional[type[Exception]],
+            exc_val: Optional[Exception],
+            exc_tb: Optional[TracebackType],
+    ) -> bool:
+        if not self._tasks:
+            return False
+
+        done, pending = await asyncio.wait(self._tasks, timeout=self._graceful_shutdown)
+        for task in done:
+            self._on_task_done(task)
+        for task in pending:
+            task.cancel()
+
+        for task in pending:
+            try:
+                await task
+            except Exception as exc:
+                logger.exception("task %s raised an exception: %s", task.get_name(), exc)
+            finally:
+                self._on_task_done(task)
+
+        return False
+
+    def create_task(self, coro: Coroutine[Any, Any, None], name: Optional[str] = None) -> asyncio.Task[None]:
+        task = asyncio.create_task(coro, name=name)
+        task.add_done_callback(self._on_task_done)
+
+        self._tasks.add(task)
+
+        return task
+
+    def _on_task_done(self, task: asyncio.Task[None]) -> None:
+        self._tasks.discard(task)

tests/test_buffer.py
@@ -1,0 +1,117 @@
+from aio.buffer import CircularBuffer, DequeBuffer
+
+
+def test_circular_buffer_capacity():
+    buffer = CircularBuffer(capacity=5)
+    assert buffer.capacity == 5
+
+
+def test_circular_buffer_size():
+    buffer = CircularBuffer(capacity=5)
+    assert buffer.size == 0
+
+    buffer.append(b'12345')
+    assert buffer.size == 5
+
+    buffer.pop(2)
+    assert buffer.size == 3
+
+    buffer.pop(3)
+    assert buffer.size == 0
+
+    buffer.pop(1)
+    assert buffer.size == 0
+
+    buffer.append(b'123')
+    assert buffer.size == 3
+
+
+def test_circular_buffer_copy():
+    buffer = CircularBuffer(capacity=5)
+    buffer.append(b'12345')
+
+    assert buffer.copy(max_bytes=1) == b'1'
+    assert buffer.copy(max_bytes=3) == b'123'
+    assert buffer.copy(max_bytes=5) == b'12345'
+    assert buffer.copy(max_bytes=6) == b'12345'
+
+
+def test_circular_buffer_append_pop():
+    buffer = CircularBuffer(capacity=5)
+    buffer.append(b'12345')
+    assert buffer.pop() == b'12345'
+
+    buffer.append(b'12345')
+    assert buffer.pop(max_bytes=2) == b'12'
+    assert buffer.pop(max_bytes=3) == b'345'
+
+    buffer.append(b'12345')
+    assert buffer.pop(max_bytes=3) == b'123'
+    buffer.append(b'678')
+    assert buffer.pop(max_bytes=3) == b'456'
+    buffer.append(b'9ab')
+    assert buffer.pop(max_bytes=1) == b'7'
+    buffer.append(b'c')
+    assert buffer.pop(max_bytes=5) == b'89abc'
+
+
+def test_deque_buffer_pop_all():
+    buffer = DequeBuffer()
+
+    buffer.append(b"0")
+    buffer.append(b"12")
+    buffer.append(b"345")
+    buffer.append(b"6789")
+    assert len(buffer) == 10
+
+    assert buffer.pop() == b"0123456789"
+
+
+def test_deque_buffer_pop_max_bytes():
+    buffer = DequeBuffer()
+
+    buffer.append(b"0")
+    buffer.append(b"12")
+    buffer.append(b"345")
+    buffer.append(b"6789")
+    assert len(buffer) == 10
+
+    assert buffer.pop(max_bytes=4) == b"0123"
+    assert len(buffer) == 6
+
+    assert buffer.pop(max_bytes=3) == b"456"
+    assert len(buffer) == 3
+
+    assert buffer.pop(max_bytes=2) == b"78"
+    assert len(buffer) == 1
+
+    assert buffer.pop(max_bytes=1) == b"9"
+    assert len(buffer) == 0
+
+    assert buffer.pop(max_bytes=1) == b""
+    assert len(buffer) == 0
+
+
+def test_deque_buffer_copy():
+    buffer = DequeBuffer()
+
+    buffer.append(b"0")
+    buffer.append(b"12")
+    buffer.append(b"345")
+    buffer.append(b"6789")
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=1) == b"0"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=2) == b"01"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=3) == b"012"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=6) == b"012345"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=10) == b"0123456789"
+    assert len(buffer) == 10

tests/test_streams.py
@@ -1,0 +1,97 @@
+import pytest
+
+from aio.buffer import BufferOverflowError, CircularBuffer
+from aio.stream import BufferedStreamReader, BufferedStreamWriter, IncompleteError
+from tests import utils
+
+
+async def test_stream_writer_write_all():
+    stream = utils.MemoryStream(write_chunk_size=3)
+
+    writer = stream
+    await writer.write_all(b"0123456789")
+
+    assert await stream.read(10) == b"0123456789"
+
+
+async def test_stream_buffered_stream_reader_read_exactly():
+    stream = utils.MemoryStream(read_chunk_size=3)
+    await stream.write(b"0123456789")
+
+    reader = BufferedStreamReader(reader=stream)
+    assert await reader.read_exactly(5) == b"01234"
+    assert await reader.read_exactly(3) == b"567"
+    assert await reader.read_exactly(2) == b"89"
+
+
+async def test_stream_buffered_stream_reader_read_exactly_incomplete():
+    stream = utils.MemoryStream(read_chunk_size=3)
+    await stream.write(b"01234")
+
+    reader = BufferedStreamReader(reader=stream)
+    with pytest.raises(IncompleteError):
+        assert await reader.read_exactly(6)
+
+    assert reader.get_buffer() == b"01234"
+
+
+async def test_stream_buffered_stream_reader_peek():
+    stream = utils.MemoryStream()
+    await stream.write(b"012345")
+
+    reader = BufferedStreamReader(reader=stream)
+    assert await reader.peek(size=6) == b"012345"
+    assert await reader.peek(size=3) == b"012"
+    assert await reader.peek(size=1) == b"0"
+
+
+async def test_stream_buffered_stream_reader_read_until():
+    stream = utils.MemoryStream(read_chunk_size=3)
+    await stream.write(b"0000100010010110")
+
+    reader = BufferedStreamReader(reader=stream)
+    assert await reader.read_until(b"1") == b"00001"
+    assert await reader.read_until(b"1") == b"0001"
+    assert await reader.read_until(b"1") == b"001"
+    assert await reader.read_until(b"1") == b"01"
+    assert await reader.read_until(b"1") == b"1"
+
+    with pytest.raises(IncompleteError):
+        await reader.read_until(b"1")
+
+    assert reader.get_buffer() == b"0"
+
+
+async def test_stream_buffered_stream_read_until_buffer_overflow():
+    stream = utils.MemoryStream(read_chunk_size=4)
+    await stream.write(b"00001")
+
+    reader = BufferedStreamReader(reader=stream, buffer=CircularBuffer(4))
+
+    with pytest.raises(BufferOverflowError):
+        await reader.read_until(b"1")
+
+    assert await reader.read_exactly(4) == b"0000"
+    assert await reader.read(1024) == b"1"
+
+
+async def test_stream_buffered_stream_read_exactly_buffer_overflow():
+    stream = utils.MemoryStream(read_chunk_size=4)
+    await stream.write(b"00001")
+
+    reader = BufferedStreamReader(reader=stream, buffer=CircularBuffer(4))
+
+    with pytest.raises(BufferOverflowError):
+        await reader.read_exactly(5)
+
+    assert await reader.read_exactly(4) == b"0000"
+    assert await reader.read(1024) == b"1"
+
+
+async def test_stream_buffered_writer_write_all():
+    stream = utils.MemoryStream(write_chunk_size=3)
+
+    writer = BufferedStreamWriter(writer=stream)
+    await writer.write_all(b"0123456789")
+
+    assert await stream.read(10) == b"0123456789"

tests/utils.py
@@ -1,0 +1,44 @@
+from typing import Optional, Union
+
+from aio.buffer import DequeBuffer
+from aio.stream import StreamReader, StreamWriter
+
+
+class StreamClosedError(Exception):
+    pass
+
+
+class MemoryStream(StreamReader, StreamWriter):
+    def __init__(self, read_chunk_size: Optional[int] = None, write_chunk_size: Optional[int] = None):
+        self._read_chunk_size = read_chunk_size
+        self._write_chunk_size = write_chunk_size
+        self._buffer = DequeBuffer()
+        self._reader_closed = False
+        self._writer_closed = False
+
+    async def read(self, max_bytes: int) -> bytes:
+        if self._reader_closed:
+            raise StreamClosedError()
+
+        return self._buffer.pop(self._read_chunk_size or max_bytes)
+
+    async def write(self, data: Union[bytes, bytearray, memoryview]) -> int:
+        if self._writer_closed:
+            raise StreamClosedError()
+
+        if self._write_chunk_size:
+            data = memoryview(data)[0:self._write_chunk_size]
+
+        self._buffer.append(data)
+
+        return len(data)
+
+    async def close_reader(self) -> None:
+        self._reader_closed = True
+
+    async def close_writer(self) -> None:
+        self._writer_closed = True
+
+    async def close(self) -> None:
+        self._reader_closed = True
+        self._writer_closed = True

dapper91/simio

examples/buffered_stream.py
@@ -1,0 +1,24 @@
+import asyncio as aio
+
+from simio import net, stream
+
+
+async def main() -> None:
+    async with await net.open_tcp_stream("httpforever.com", 80) as http_stream:
+        buffered_stream = stream.BufferedStream(http_stream)
+        await buffered_stream.write_all(
+            b'GET / HTTP/1.1\r\n'
+            b'Host: httpforever.com\r\n'
+            b'\r\n',
+        )
+        status = await buffered_stream.read_until(b'\r\n')
+        print(status.decode())
+
+        headers = await buffered_stream.read_until(b'\r\n\r\n')
+        headers = dict(line.split(b':', maxsplit=1) for line in headers.removesuffix(b'\r\n\r\n').split(b'\r\n'))
+
+        body = await buffered_stream.read_exactly(size=int(headers[b'Content-Length']))
+        print(body.decode())
+
+
+aio.run(main())

simio/net/tcp/server.py
@@ -1,0 +1,112 @@
+import asyncio as aio
+import logging
+import socket as sc
+from types import TracebackType
+from typing import Any, Callable, Coroutine, Optional, Self
+
+from simio.net import utils
+
+logger = logging.getLogger(__package__)
+
+
+async def start_tcp_server(
+        host: str,
+        port: int,
+        handler: Callable[[sc.socket], Coroutine[None, None, None]],
+        *,
+        family: sc.AddressFamily = sc.AddressFamily.AF_INET,
+        backlog: int = 128,
+        reuse_address: bool = True,
+        graceful_shutdown: Optional[float] = None,
+) -> None:
+    """
+    Starts TCP socket server.
+
+    :param host: address to bind to
+    :param port: port to bind to
+    :param handler: client socket handler
+    :param family: address family
+    :param backlog: number of unaccepted connections that the system will allow before refusing new connections
+    :param reuse_address: reuse bind address
+    :param graceful_shutdown: period of time during which the server waits for handlers to complete
+                              before canceling them
+    """
+
+    loop = aio.get_running_loop()
+
+    logger.info("starting server on %s:%d", host, port)
+
+    with sc.socket(family, sc.SOCK_STREAM.SOCK_STREAM) as srv_socket:
+        if reuse_address:
+            srv_socket.setsockopt(sc.SOL_SOCKET, sc.SO_REUSEADDR, 1)
+
+        srv_socket.setblocking(False)
+        srv_socket.bind((host, port))
+        srv_socket.listen(backlog)
+
+        async with TaskGuard(graceful_shutdown=graceful_shutdown) as handlers:
+            while True:
+                try:
+                    cli_socket, address = await loop.sock_accept(srv_socket)
+                except aio.CancelledError:
+                    break
+
+                handlers.create_task(_handle_client_socket(handler, cli_socket))
+
+
+async def _handle_client_socket(handler: Callable[[sc.socket], Coroutine[None, None, None]], socket: sc.socket) -> None:
+    remote_host, remote_port = utils.get_socket_peer_address(socket)
+    logger.info("client %s:%d connected", remote_host, remote_port)
+
+    with socket:
+        try:
+            await handler(socket)
+        except Exception as e:
+            logger.exception("client %s:%d handling error: %s", remote_host, remote_port, e)
+
+    logger.info("client %s:%d disconnected", remote_host, remote_port)
+
+
+class TaskGuard:
+    def __init__(self, graceful_shutdown: Optional[float] = None):
+        self._graceful_shutdown = graceful_shutdown
+        self._tasks: set[aio.Task[None]] = set()
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+            self,
+            exc_type: Optional[type[Exception]],
+            exc_val: Optional[Exception],
+            exc_tb: Optional[TracebackType],
+    ) -> bool:
+        if not self._tasks:
+            return False
+
+        done, pending = await aio.wait(self._tasks, timeout=self._graceful_shutdown)
+        for task in done:
+            self._on_task_done(task)
+        for task in pending:
+            task.cancel()
+
+        for task in pending:
+            try:
+                await task
+            except Exception as exc:
+                logger.exception("task %s raised an exception: %s", task.get_name(), exc)
+            finally:
+                self._on_task_done(task)
+
+        return False
+
+    def create_task(self, coro: Coroutine[Any, Any, None], name: Optional[str] = None) -> aio.Task[None]:
+        task = aio.create_task(coro, name=name)
+        task.add_done_callback(self._on_task_done)
+
+        self._tasks.add(task)
+
+        return task
+
+    def _on_task_done(self, task: aio.Task[None]) -> None:
+        self._tasks.discard(task)

tests/test_buffer.py
@@ -1,0 +1,117 @@
+from simio.buffer import CircularBuffer, DequeBuffer
+
+
+def test_circular_buffer_capacity():
+    buffer = CircularBuffer(capacity=5)
+    assert buffer.capacity == 5
+
+
+def test_circular_buffer_size():
+    buffer = CircularBuffer(capacity=5)
+    assert buffer.size == 0
+
+    buffer.append(b'12345')
+    assert buffer.size == 5
+
+    buffer.pop(2)
+    assert buffer.size == 3
+
+    buffer.pop(3)
+    assert buffer.size == 0
+
+    buffer.pop(1)
+    assert buffer.size == 0
+
+    buffer.append(b'123')
+    assert buffer.size == 3
+
+
+def test_circular_buffer_copy():
+    buffer = CircularBuffer(capacity=5)
+    buffer.append(b'12345')
+
+    assert buffer.copy(max_bytes=1) == b'1'
+    assert buffer.copy(max_bytes=3) == b'123'
+    assert buffer.copy(max_bytes=5) == b'12345'
+    assert buffer.copy(max_bytes=6) == b'12345'
+
+
+def test_circular_buffer_append_pop():
+    buffer = CircularBuffer(capacity=5)
+    buffer.append(b'12345')
+    assert buffer.pop() == b'12345'
+
+    buffer.append(b'12345')
+    assert buffer.pop(max_bytes=2) == b'12'
+    assert buffer.pop(max_bytes=3) == b'345'
+
+    buffer.append(b'12345')
+    assert buffer.pop(max_bytes=3) == b'123'
+    buffer.append(b'678')
+    assert buffer.pop(max_bytes=3) == b'456'
+    buffer.append(b'9ab')
+    assert buffer.pop(max_bytes=1) == b'7'
+    buffer.append(b'c')
+    assert buffer.pop(max_bytes=5) == b'89abc'
+
+
+def test_deque_buffer_pop_all():
+    buffer = DequeBuffer()
+
+    buffer.append(b"0")
+    buffer.append(b"12")
+    buffer.append(b"345")
+    buffer.append(b"6789")
+    assert len(buffer) == 10
+
+    assert buffer.pop() == b"0123456789"
+
+
+def test_deque_buffer_pop_max_bytes():
+    buffer = DequeBuffer()
+
+    buffer.append(b"0")
+    buffer.append(b"12")
+    buffer.append(b"345")
+    buffer.append(b"6789")
+    assert len(buffer) == 10
+
+    assert buffer.pop(max_bytes=4) == b"0123"
+    assert len(buffer) == 6
+
+    assert buffer.pop(max_bytes=3) == b"456"
+    assert len(buffer) == 3
+
+    assert buffer.pop(max_bytes=2) == b"78"
+    assert len(buffer) == 1
+
+    assert buffer.pop(max_bytes=1) == b"9"
+    assert len(buffer) == 0
+
+    assert buffer.pop(max_bytes=1) == b""
+    assert len(buffer) == 0
+
+
+def test_deque_buffer_copy():
+    buffer = DequeBuffer()
+
+    buffer.append(b"0")
+    buffer.append(b"12")
+    buffer.append(b"345")
+    buffer.append(b"6789")
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=1) == b"0"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=2) == b"01"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=3) == b"012"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=6) == b"012345"
+    assert len(buffer) == 10
+
+    assert buffer.copy(max_bytes=10) == b"0123456789"
+    assert len(buffer) == 10

tests/test_streams.py
@@ -1,0 +1,97 @@
+import pytest
+
+from simio.buffer import BufferOverflowError, CircularBuffer
+from simio.stream import BufferedStreamReader, BufferedStreamWriter, IncompleteError
+from tests import utils
+
+
+async def test_stream_writer_write_all():
+    stream = utils.MemoryStream(write_chunk_size=3)
+
+    writer = stream
+    await writer.write_all(b"0123456789")
+
+    assert await stream.read(10) == b"0123456789"
+
+
+async def test_stream_buffered_stream_reader_read_exactly():
+    stream = utils.MemoryStream(read_chunk_size=3)
+    await stream.write(b"0123456789")
+
+    reader = BufferedStreamReader(reader=stream)
+    assert await reader.read_exactly(5) == b"01234"
+    assert await reader.read_exactly(3) == b"567"
+    assert await reader.read_exactly(2) == b"89"
+
+
+async def test_stream_buffered_stream_reader_read_exactly_incomplete():
+    stream = utils.MemoryStream(read_chunk_size=3)
+    await stream.write(b"01234")
+
+    reader = BufferedStreamReader(reader=stream)
+    with pytest.raises(IncompleteError):
+        assert await reader.read_exactly(6)
+
+    assert reader.get_buffer() == b"01234"
+
+
+async def test_stream_buffered_stream_reader_peek():
+    stream = utils.MemoryStream()
+    await stream.write(b"012345")
+
+    reader = BufferedStreamReader(reader=stream)
+    assert await reader.peek(size=6) == b"012345"
+    assert await reader.peek(size=3) == b"012"
+    assert await reader.peek(size=1) == b"0"
+
+
+async def test_stream_buffered_stream_reader_read_until():
+    stream = utils.MemoryStream(read_chunk_size=3)
+    await stream.write(b"0000100010010110")
+
+    reader = BufferedStreamReader(reader=stream)
+    assert await reader.read_until(b"1") == b"00001"
+    assert await reader.read_until(b"1") == b"0001"
+    assert await reader.read_until(b"1") == b"001"
+    assert await reader.read_until(b"1") == b"01"
+    assert await reader.read_until(b"1") == b"1"
+
+    with pytest.raises(IncompleteError):
+        await reader.read_until(b"1")
+
+    assert reader.get_buffer() == b"0"
+
+
+async def test_stream_buffered_stream_read_until_buffer_overflow():
+    stream = utils.MemoryStream(read_chunk_size=4)
+    await stream.write(b"00001")
+
+    reader = BufferedStreamReader(reader=stream, buffer=CircularBuffer(4))
+
+    with pytest.raises(BufferOverflowError):
+        await reader.read_until(b"1")
+
+    assert await reader.read_exactly(4) == b"0000"
+    assert await reader.read(1024) == b"1"
+
+
+async def test_stream_buffered_stream_read_exactly_buffer_overflow():
+    stream = utils.MemoryStream(read_chunk_size=4)
+    await stream.write(b"00001")
+
+    reader = BufferedStreamReader(reader=stream, buffer=CircularBuffer(4))
+
+    with pytest.raises(BufferOverflowError):
+        await reader.read_exactly(5)
+
+    assert await reader.read_exactly(4) == b"0000"
+    assert await reader.read(1024) == b"1"
+
+
+async def test_stream_buffered_writer_write_all():
+    stream = utils.MemoryStream(write_chunk_size=3)
+
+    writer = BufferedStreamWriter(writer=stream)
+    await writer.write_all(b"0123456789")
+
+    assert await stream.read(10) == b"0123456789"

tests/utils.py
@@ -1,0 +1,44 @@
+from typing import Optional, Union
+
+from simio.buffer import DequeBuffer
+from simio.stream import StreamReader, StreamWriter
+
+
+class StreamClosedError(Exception):
+    pass
+
+
+class MemoryStream(StreamReader, StreamWriter):
+    def __init__(self, read_chunk_size: Optional[int] = None, write_chunk_size: Optional[int] = None):
+        self._read_chunk_size = read_chunk_size
+        self._write_chunk_size = write_chunk_size
+        self._buffer = DequeBuffer()
+        self._reader_closed = False
+        self._writer_closed = False
+
+    async def read(self, max_bytes: int) -> bytes:
+        if self._reader_closed:
+            raise StreamClosedError()
+
+        return self._buffer.pop(self._read_chunk_size or max_bytes)
+
+    async def write(self, data: Union[bytes, bytearray, memoryview]) -> int:
+        if self._writer_closed:
+            raise StreamClosedError()
+
+        if self._write_chunk_size:
+            data = memoryview(data)[0:self._write_chunk_size]
+
+        self._buffer.append(data)
+
+        return len(data)
+
+    async def close_reader(self) -> None:
+        self._reader_closed = True
+
+    async def close_writer(self) -> None:
+        self._writer_closed = True
+
+    async def close(self) -> None:
+        self._reader_closed = True
+        self._writer_closed = True